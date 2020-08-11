be4a670446a399eab1a4da687358159101fcb0ed
john doe <scottbruceburton@gmail.com>
2020-08-11 07:07:07 -0400

    added TODOs

--- getfilesize.py
+++ getfilesize.py
@@ -6,8 +6,11 @@ import os
 
 totalSize = 0
 
+#TODO Add Error Check for non-existing directory
 directory = input('Enter the directory you want sized:\n')
 
+#TODO Enable Sub-directory Sizing (Recursion?!)
+# currently only sizes main folder
 for filename in os.listdir(directory):
     totalSize += os.path.getsize(os.path.join(directory, filename))
 
